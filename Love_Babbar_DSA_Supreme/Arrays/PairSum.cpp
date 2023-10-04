35e13491ca80d5468cbc3a6137ff9f3898e69e42
Swati Ranjan <swatirp83@gmail.com>
2023-10-04 09:55:13 +0530

    practice

--- Love_Babbar_DSA_Supreme/Arrays/PairSum.cpp
+++ Love_Babbar_DSA_Supreme/Arrays/PairSum.cpp
@@ -1,18 +1,43 @@
+
+//TIME COMPLEXITY FOR THIS APPROACH IS O(n * log(n))
 #include <iostream>
 #include <vector>
+#include<algorithm>
 using namespace std;
 
+void pairSum(vector<int> arr,int n,int sum){
+    sort(arr.begin(),arr.end());
+   for(int i=0;i<n;i++){
+       if(arr[i]+arr[i+1] == sum){
+           cout<<arr[i]<<" "<<arr[i+1]<<endl;
+       }
+   }
+}
 int main() {
-  int arr[]={1,3,5,7,9};
-  int size1=5;
+  vector<int> v{1,3,5,7,9};
+  int n = v.size();
   int sum=8;
+ 
+  pairSum(v,n,sum);
+}
 
-  for(int i=0;i<size1;i++){
-   for(int j=i+1;j<size1;j++){
-     if(arr[i]+arr[j]==sum){
-       cout<<arr[i]<<" "<<arr[j]<<endl;
+// TIME COMPLEXITY FOR THIS APPROACH IS: O(n^2)
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//   int arr[]={1,3,5,7,9};
+//   int size1=5;
+//   int sum=8;
+
+//   for(int i=0;i<size1;i++){
+//    for(int j=i+1;j<size1;j++){
+//      if(arr[i]+arr[j]==sum){
+//        cout<<arr[i]<<" "<<arr[j]<<endl;
        
 
-     }
-   }
-  }
+//      }
+//    }
+//   }
+// }
