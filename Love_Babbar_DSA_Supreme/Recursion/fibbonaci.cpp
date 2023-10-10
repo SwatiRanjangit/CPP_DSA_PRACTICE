37b47b11f92b4fd3b1591994412cf4013220e791
Swati Ranjan <swatirp83@gmail.com>
2023-10-10 13:02:32 +0530

    recursion

---
+++ Love_Babbar_DSA_Supreme/Recursion/fibbonaci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int fibbonaci(int n){
+  if(n==1){
+    return 0;
+  }
+  if(n==2){
+    return 1;
+  }
+  int ans = fibbonaci(n-1) + fibbonaci(n-2);
+  return ans;
+}
+
+int main() {
+  int n;
+  cin>>n;
+  int ans = fibbonaci(n);
+  cout<<ans<<endl;
+
+  return 0;
+}
