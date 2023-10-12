be085e9f960fe1bcdd4bcbc72460d3f23eddf3eb
Swati Ranjan <swatirp83@gmail.com>
2023-10-12 08:48:48 +0530

    recursion day-3

--- Love_Babbar_DSA_Supreme/Recursion/lastOccurence.cpp
+++ Love_Babbar_DSA_Supreme/Recursion/lastOccurence.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int lastOcc(string& s,int i,char key, int& ans){
-  if(s[i] == '\0') return 0;
+// int lastOcc(string& s,int i,char key, int& ans){
+//   // left to right
+//   if(s[i] == '\0') return 0;
+  
+//   if(s[i]==key) {
+//     ans = i;
+//   }
+
+//   lastOcc(s,i+1,key,ans);
+
+//   return ans;
+ 
+// }
+void lastOcc(string& s,int i,char key, int& ans){
+  //  right to left 
+  if(i<0) return;
   
   if(s[i]==key) {
     ans = i;
+    return;
   }
 
-  lastOcc(s,i+1,key,ans);
+  lastOcc(s,i-1,key,ans);
 
-  return ans;
+  
  
 }
 int main() {
-  string s = "abbbbbba";
-  int i=0;
+  string s = "rbbbbbbf";
+  int i = s.length() - 1; // Start from the end of the string
   char key='b';
   int ans = -1;
-  int answer = lastOcc(s,i,key,ans);
-  cout<<"Last occurence of character in string is: "<<answer<<endl;
+ lastOcc(s,i,key,ans);
+  cout<<"Last occurence of character in string is: "<<ans<<endl;
 
   return 0;
 }
