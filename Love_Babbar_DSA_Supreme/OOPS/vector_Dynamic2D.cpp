e91846966614621eb9d824f45b8299230a386d9c
Swati Ranjan <swatirp83@gmail.com>
2023-11-02 17:46:54 +0530

    OOPS DAY-1

---
+++ Love_Babbar_DSA_Supreme/OOPS/vector_Dynamic2D.cpp
@@ -0,0 +1,17 @@
+// Dynamic memo allocation of 2D Array using vector
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>>arr(5,vector<int>(6,0));
+   // printing
+   for(int i=0; i<5;i++){
+       for(int j=0; j<6;j++){
+           cout<<arr[i][j]<<" ";
+       }
+       cout<<endl;
+   }
+
+    return 0;
+}
