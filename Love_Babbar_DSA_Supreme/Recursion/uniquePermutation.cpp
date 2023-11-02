1c672385711201a3a26cc3650d1afb8edadd7606
Swati Ranjan <swatirp83@gmail.com>
2023-11-02 10:09:49 +0530

    my interview que done

--- Love_Babbar_DSA_Supreme/Recursion/uniquePermutation.cpp
+++ Love_Babbar_DSA_Supreme/Recursion/uniquePermutation.cpp
@@ -10,11 +10,16 @@ void permuteUniqueHelper(vector<int>& nums, vector<vector<int>>& ans, int start)
         ans.push_back(nums);
         return;
     }
+ unordered_map<int,bool>visited;
 
-    for (int i = start; i < nums.size(); i++) {
-        swap(nums[i], nums[start]);
-        permuteUniqueHelper(nums, ans, start + 1);
-        swap(nums[i], nums[start]);
+    for(int i=start; i< nums.size();i++){
+        if(visited.find(nums[i]) != visited.end()){
+            continue;
+        }
+        visited[nums[i]]=true;
+        swap(nums[i],nums[start]);
+        permuteUniqueHelper(nums, ans, start+1);
+        swap(nums[i],nums[start]);
     }
 }
 
