0490c049063e370d095cf552bcad1c80bf172cbd
Swati Ranjan <swatirp83@gmail.com>
2023-11-03 08:25:17 +0530

    OOPS DAY-2

---
+++ Love_Babbar_DSA_Supreme/OOPS/staticKeyword.cpp
@@ -0,0 +1,30 @@
+// 
+#include <iostream>
+using namespace std;
+
+class abc{
+    public:
+    static int x,y; // accessible to all the instances created only one copy of this will be provided to all instaces
+    
+    void print(){
+        cout<<x<<" "<<y<<endl;
+    }
+};
+
+
+// if we use static varible we have to declare it outside for using it.
+int abc::x;
+int abc::y;
+
+int main() {
+    abc obj1;
+    obj1.x=1;
+    obj1.y=2;
+    obj1.print();
+    abc obj2;
+    obj2.x=10;
+    obj2.y=30;
+    obj1.print();
+    obj2.print();
+    return 0;
+}
