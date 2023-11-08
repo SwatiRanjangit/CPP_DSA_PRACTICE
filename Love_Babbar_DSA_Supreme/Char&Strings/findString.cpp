7d8bd3e1cc92f01a45db19346517bfefd3a71331
Swati Ranjan <swatirp83@gmail.com>
2023-11-08 07:42:52 +0530

    Linked-list Day-2

--- Love_Babbar_DSA_Supreme/Char&Strings/findString.cpp
+++ Love_Babbar_DSA_Supreme/Char&Strings/findString.cpp
@@ -2,20 +2,6 @@
 #include <cstring>
 using namespace std;
 
-// bool compareString(string a, string b) {
-//   if(a.length() != b.length()) return false;
-  
-//   int j=0;
-//   for(int i=0;i<a.length();i++){
-//     if(a[i] != b[i]){
-//       return false;
-//     }else{
-//       j++;
-//     }
-//   }
-//   return true;
-// }
-
 int main() {
   string sentence="i am swati ranjan";
   string target = "tiya";
@@ -25,13 +11,6 @@ int main() {
   }else{
     cout<<"found"<<endl;
   }
-
-  // bool ans =compareString(a,b);
-  //  if(ans){
-  //    cout<<"equal"<<endl;
-  //  }else{
-  //    cout<<"not equal"<<endl;
-  //  }
 
 
   return 0;
